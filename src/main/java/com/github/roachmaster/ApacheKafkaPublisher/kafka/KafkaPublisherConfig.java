d86cd11fbdd5629a3ea9950d24b9e3ff0b07265d
Leonardo <leonardo.rocha1990@gmail.com>
2022-01-23 19:04:41 -0600

    Adding publisher classes

--- src/main/java/com/github/roachmaster/ApacheKafkaPublisher/kafka/KafkaPublisherConfig.java
+++ src/main/java/com/github/roachmaster/ApacheKafkaPublisher/kafka/KafkaPublisherConfig.java
@@ -2,16 +2,22 @@ package com.github.roachmaster.ApacheKafkaPublisher.kafka;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableKafka
 public class KafkaPublisherConfig {
 
     @Value("${kafkaTopic}")
@@ -29,6 +35,22 @@ public class KafkaPublisherConfig {
     public KafkaAdmin admin(){
         Map<String, Object> configs = new HashMap<>();
         configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "kube2:30092");
+        configs.put(AdminClientConfig.RETRY_BACKOFF_MS_CONFIG,"1000");
         return new KafkaAdmin(configs);
+    }
+    @Bean
+    public ProducerFactory<Integer, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(senderProps());
+    }
+
+    private Map<String, Object> senderProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kube2:30092");
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 10);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, "1000");
+        props.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, "300000");
+        return props;
     }
 }
